87fc2419a05e967fee5f4c3dbcd6fe9a284a0e56
Tommy Le <teknoma104@gmail.com>
2018-07-03 21:23:26 -0500

    Fixed loop, game now shows question + answer but answer still needs to be fixed because it shows up as object instead of the actual value.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,32 @@ var instructionText = "You will get 30 seconds to answer each question. There ar
 
 var difficulty = "";
 var questionsArray = [{ q: "", ca: "", ia: [] }];
+var questionCards = [];
+
+var questionContainer = $("#question-container");
 
 // The createRow function takes data returned by OMDB and appends the table data to the tbody
-var createQA = function (data) {
-    // Get reference to existing tbody element, create a new table row element
-    var tBody = $("tbody");
-    var tRow = $("<tr>");
+// var createQA = function (data) {
+//     // Get reference to existing tbody element, create a new table row element
+//     var tBody = $("tbody");
+//     var tRow = $("<tr>");
 
-    // Methods run on jQuery selectors return the selector they we run on
-    // This is why we can create and save a reference to a td in the same statement we update its text
-    var titleTd = $("<td>").text(data.Title);
-    var yearTd = $("<td>").text(data.Year);
-    var actorsTd = $("<td>").text(data.Actors);
-    // Append the newly created table data to the table row
-    tRow.append(titleTd, yearTd, actorsTd);
-    // Append the table row to the table body
-    tBody.append(tRow);
-};
+//     // Methods run on jQuery selectors return the selector they we run on
+//     // This is why we can create and save a reference to a td in the same statement we update its text
+//     var titleTd = $("<td>").text(data.Title);
+//     var yearTd = $("<td>").text(data.Year);
+//     var actorsTd = $("<td>").text(data.Actors);
+//     // Append the newly created table data to the table row
+//     tRow.append(titleTd, yearTd, actorsTd);
+//     // Append the table row to the table body
+//     tBody.append(tRow);
+// };
 
 // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
-var retrieveQuestions = function (mode) {
+var retrieveAndParseQA = function (mode) {
     var queryURL = "https://opentdb.com/api.php?amount=10&category=15&difficulty=" + mode + "";
+
+
 
     console.log(queryURL);
 
@@ -31,10 +36,7 @@ var retrieveQuestions = function (mode) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        createQA(response);
         console.log(response);
-
-        var obj = {};
 
         console.log("Total # of responses: " + response.results.length);
         console.log(response.results[0].question);
@@ -54,7 +56,8 @@ var retrieveQuestions = function (mode) {
 
             questionsArray[x] = {
                 "q": response.results[x].question,
-                "ca": response.results[x].correct_answer
+                "ca": response.results[x].correct_answer,
+                "ia": []
             }
 
             console.log("Testing questionArray question: " + questionsArray[x].q);
@@ -62,46 +65,97 @@ var retrieveQuestions = function (mode) {
 
             console.log("There are " + response.results[x].incorrect_answers.length + " incorrect answers.");
 
-            var wrongAnswers = [];
             for (var y = 0; y < response.results[x].incorrect_answers.length; y++) {
                 console.log("Starting y loop: number " + y);
                 console.log("Trying to assign questionsArray[" + x + "].ic[" + y + "] the value");
-                console.log("from respone.results[" + x + "].incorrect_answers[" + y + "] which is " + response.results[x].incorrect_answers[y]);
+                console.log("from response.results[" + x + "].incorrect_answers[" + y + "] which is " + response.results[x].incorrect_answers[y]);
                 console.log(response.results[x].incorrect_answers[y]);
-                wrongAnswers[y] = response.results[x].incorrect_answers[y];
-                questionsArray[x]["ia"][y] =response.results[x].incorrect_answers[y];
+
+                questionsArray[x].ia[y] = response.results[x].incorrect_answers[y];
             }
 
-            
-            
-            console.log("Testing incorrect answer array: " + questionsArray.ia[0]);
+            console.log("Testing incorrect answer array: " + questionsArray[x].ia[0]);
+
+
+            questionCards.push(createCard(questionsArray[x]));
             console.log("================= End Loop  =================");
         }
 
+        questionContainer.append(questionCards);
 
 
-
-    }
-
-
-
-
-
-
-        // $.when.apply($, questionsArray.map(function(queryURL) {
-        //     return $.ajax(queryURL);
-        // })).done(function() {
-        //     var results = [];
-
-        //     for (var x = 0; x < arguments.length; x++) {
-        //         results.push(arguments[x][0]);
-        //         console.log("Pushing " + arguments[x][0]);
-        //     }
-        // })
-    )
+    })
 };
 
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
 
+function createCard(article) {
+    // This function takes in a single JSON object for an article/headline
+    // It constructs a jQuery element containing all of the formatted HTML for the
+    // article card
+
+    console.log("createCard function called");
+    var answersCombined = [];
+
+    console.log("question: " + article.q);
+
+    var card = $("<div class='card'>");
+    var cardHeader = $("<div class='card-header'>").append($("<h3>").append(article.q));
+
+    console.log("correct answer: " + article.ca);
+    console.log("incorrect answer: " + article.ia);
+
+    answersCombined.push(article.ca);
+    console.log("Showing answersCombined after pushing article.ca:  " + answersCombined);
+
+    if (article.ia.length === 1)
+        answersCombined.push(article.ia);
+    else 
+    {
+        for (var x = 0; x < article.ia.length; x++ )
+        answersCombined.push(article.ia[x]);
+    }
+    console.log("Showing answersCombined after pushing article.ia:  " + answersCombined);
+
+    for (var x = 0; x < answersCombined.length; x++) {
+        console.log("Loop #" + x + " to see value of answerCombined[" + x + "]!");
+        console.log(answersCombined[x]);
+    }
+
+    var unorderedList = $("<ul>");
+
+    if (answersCombined.length < 3) {
+        console.log("answersCombined.length is : " + answersCombined.length);
+        console.log("Conditional met for having only 2 possible answers.");
+        var cardBody = $("<div class='card-body'>").html(unorderedList.text(answersCombined[0]) + unorderedList.text(answersCombined[1]));
+    }
+    else {
+        console.log("Conditional met for having 4 possible answers.");
+        console.log("answersCombined preshuffle:  " + answersCombined);
+        shuffle(answersCombined);
+        console.log("answersCombined shuffled now:  " + answersCombined);
+        
+
+        var cardBody = $("<div class='card-body'>").html(
+            unorderedList.text(answersCombined[0]) +
+            unorderedList.text(answersCombined[1]) +
+            unorderedList.text(answersCombined[2]) +
+            unorderedList.text(answersCombined[3]));
+    }
+
+    card.append(cardHeader, cardBody);
+
+    return card;
+}
 
 $(document).ready(function () {
     $("#info-box").html(startGameText);
@@ -119,7 +173,7 @@ $(document).ready(function () {
         console.log("User click easy mode");
         difficulty = "easy";
         console.log("Difficulty now set to: " + difficulty);
-        retrieveQuestions(difficulty);
+        retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
         $("#mediumMode").addClass("initiallyHidden");
@@ -130,7 +184,7 @@ $(document).ready(function () {
         console.log("User click medium mode");
         difficulty = "medium";
         console.log("Difficulty now set to: " + difficulty);
-        retrieveQuestions(difficulty);
+        retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
         $("#mediumMode").addClass("initiallyHidden");
@@ -141,7 +195,7 @@ $(document).ready(function () {
         console.log("User click hard mode");
         difficulty = "hard";
         console.log("Difficulty now set to: " + difficulty);
-        retrieveQuestions(difficulty);
+        retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
         $("#mediumMode").addClass("initiallyHidden");
