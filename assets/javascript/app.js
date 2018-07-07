16583782b201a45d9b60fe668f6ef1f972635541
Tommy Le <teknoma104@gmail.com>
2018-07-07 00:09:53 -0500

    Game mostly done. Now with sounds/gifs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,33 +16,84 @@ var timerRemaining = 30;
 
 var questionContainer = $("#question-container");
 
+var winThemes = [
+    "./assets/sounds/win/12_applause.wav.mp3",
+    "./assets/sounds/win/17 Extra Life.mp3",
+    "./assets/sounds/win/18 - 1-Up.mp3",
+    "./assets/sounds/win/23 Star Catch Fanfare.mp3",
+    "./assets/sounds/win/24 - Stage Clear.mp3",
+    "./assets/sounds/win/119 victory.mp3",
+    "./assets/sounds/win/FF7 AC Victory Fanfare Ringtone (perfected mp3).mp3",
+    "./assets/sounds/win/KirbyDance.wav"
+];
 
-function decodeSpecialChars(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
+var winGifs = [
+    "./assets/images/win/donkeykongwin.gif",
+    "./assets/images/win/ff8win.gif",
+    "./assets/images/win/sakurawin.gif",
+    "./assets/images/win/sm64victory.gif",
+    "./assets/images/win/spinalwin.gif"
+];
+
+var loseThemes = [
+    "./assets/sounds/lose/15-player down.mp3",
+    "./assets/sounds/lose/18 - Game Over (alternate).mp3",
+    "./assets/sounds/lose/25 - Lose.mp3",
+    "./assets/sounds/lose/45. Stage-End.mp3",
+    "./assets/sounds/lose/120 defeat.mp3",
+    "./assets/sounds/lose/209 - player down.mp3"
+];
+
+var loseGifs = [
+    "./assets/images/lose/bisongameover.gif",
+    "./assets/images/lose/crashbandicootlose.gif",
+    "./assets/images/lose/donkeykonglose.gif",
+    "./assets/images/lose/finalfightgameover.gif",
+    "./assets/images/lose/frygameover.gif",
+    "./assets/images/lose/spidermangameover.gif",
+    "./assets/images/lose/weirdgameover.gif"
+];
+
+var playWinTheme = function () {
+    var index = Math.floor(Math.random() * (winThemes.length));
+    var thisSound = new Audio(winThemes[index]);
+    thisSound.play();
 }
+
+var playLoseTheme = function () {
+    var index = Math.floor(Math.random() * (loseThemes.length));
+    var thisSound = new Audio(loseThemes[index]);
+    thisSound.play();
+}
+
+var displayWinGIF = function () {
+    var index = Math.floor(Math.random() * (winGifs.length));
+    var thisPic = winGifs[index];
+    var winIMG = $("<img>");
+
+    $("#question-container").empty();
+    $("#question-container").html(winIMG.attr("src", thisPic));
+}
+
+var displayLoseGIF = function () {
+    var index = Math.floor(Math.random() * (loseGifs.length));
+    var thisPic = loseGifs[index];
+    var loseIMG = $("<img>");
+
+    $("#question-container").empty();
+    $("#question-container").html(loseIMG.attr("src", thisPic));
+
+}
+
 
 var retrieveAndParseQA = function (mode) {
     var queryURL = "https://opentdb.com/api.php?amount=10&category=15&difficulty=" + mode + "";
-
-    console.log("retrieveAndParseQA function called");
-    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-
-        console.log("Total # of responses: " + response.results.length);
-
-        console.log("Starting for loop to add the questions+answers into questionsArray");
         for (var x = 0; x < response.results.length; x++) {
-            console.log("================= Loop " + x + " =================");
-            console.log("The question is: " + response.results[x].question);
-            console.log("The correct answer is: " + response.results[x].correct_answer);
-            console.log("There are " + response.results[x].incorrect_answers.length + " incorrect answers.");
 
             questionsArray[x] = {
                 "question": response.results[x].question,
@@ -51,58 +102,40 @@ var retrieveAndParseQA = function (mode) {
             }
 
 
-            console.log("--------------------");
-            console.log("Looking for &quot; in question");
-            console.log("Before the change:  " + response.results[x].question);
-            var checkQuotesQuestion = response.results[x].question;
-            var textQuestion = decodeURIComponent(response.results[x].question);
-            var decodeQuestion = response.results[x].question;
-            var hedecodeQuestion = response.results[x].question;
-
-            checkQuotesQuestion = checkQuotesQuestion.replace(/&quot;/g, '"');
-            decodeSpecialChars(decodeQuestion);
-            he.decode(hedecodeQuestion);
-
-            console.log("checkQuotesQuestion:  " + checkQuotesQuestion);
-            console.log("textQuestion:  " + textQuestion);
-            console.log("decodeQuestion:  " + decodeQuestion);
-            console.log("hedecodeQuestion:  " + hedecodeQuestion);
-
+            // Still unfinished section here that is not being used
+            // I was testing replace and decodeURIComponent here to try to fix
+            // the HTML entity encoding that the opentriviaDB API sends back in its response
+            // I only got the replace method to work so that means I needed to add a bunch of replace
+            // methods to try to catch all the various special characters which isn't something
+            // I really wanted to do, was hoping decodeURIComponent would work
+            // console.log("--------------------");
+            
+            // console.log("Looking for &quot; in question");
+            // console.log("Before the change:  " + response.results[x].question);
+            // var checkQuotesQuestion = response.results[x].question;
+            // var textQuestion = decodeURIComponent(response.results[x].question);
+            // checkQuotesQuestion = checkQuotesQuestion.replace(/&quot;/g, '"');
+            // console.log("checkQuotesQuestion:  " + checkQuotesQuestion);
+            // console.log("textQuestion:  " + textQuestion);
 
 
-            console.log("Looking for &quot; in correct_answer");
-            console.log("Before the change:  " + response.results[x].correct_answer);
-            var checkQuotesCorrectAnswer = response.results[x].correct_answer;
-            var textCorrectAnswer = decodeURIComponent(response.results[x].correct_answer);
-            var decodeCorrectAnswer = response.results[x].correct_answer;
-            var hedecodeCorrectAnswer = response.results[x].correct_answer;
-
-            checkQuotesCorrectAnswer = checkQuotesCorrectAnswer.replace(/&quot;/g, '"');
-            decodeSpecialChars(decodeCorrectAnswer);
-            he.decode(hedecodeCorrectAnswer);
-
-            console.log("checkQuotesCorrectAnswer:  " + checkQuotesCorrectAnswer);
-            console.log("textCorrectAnswer:  " + textCorrectAnswer);
-            console.log("decodeCorrectAnswer:  " + decodeCorrectAnswer);
-            console.log("hedecodeCorrectAnswer:  " + hedecodeCorrectAnswer);
-            console.log("--------------------");
+            // console.log("Looking for &quot; in correct_answer");
+            // console.log("Before the change:  " + response.results[x].correct_answer);
+            // var checkQuotesCorrectAnswer = response.results[x].correct_answer;
+            // var textCorrectAnswer = decodeURIComponent(response.results[x].correct_answer);
+            // checkQuotesCorrectAnswer = checkQuotesCorrectAnswer.replace(/&quot;/g, '"');
+            // console.log("checkQuotesCorrectAnswer:  " + checkQuotesCorrectAnswer);
+            // console.log("textCorrectAnswer:  " + textCorrectAnswer);
+            // console.log("decodeCorrectAnswer:  " + decodeCorrectAnswer);
+            
+            // console.log("--------------------");
 
 
-            console.log("Testing questionArray question: " + questionsArray[x].question);
-            console.log("Testing questionArray correct answer: " + questionsArray[x].correctAnswer);
 
-            console.log("There are " + response.results[x].incorrect_answers.length + " incorrect answers.");
 
             for (var y = 0; y < response.results[x].incorrect_answers.length; y++) {
-                console.log("Starting y loop: number " + y);
-                console.log("Incorrect answer #" + y + ": " + response.results[x].incorrect_answers[y]);
-
                 questionsArray[x].incorrectAnswer[y] = response.results[x].incorrect_answers[y];
             }
-
-            console.log("Testing incorrect answer array: " + questionsArray[x].incorrectAnswer);
-
-            console.log("================= End Loop  =================");
         }
     }).then(function () {
         displayQuestions();
@@ -113,8 +146,6 @@ var retrieveAndParseQA = function (mode) {
 
 
 function displayQuestions() {
-    console.log("displayQuestions function called");
-
     timerRemaining = 30;
 
     $("#question-container").empty();
@@ -125,16 +156,12 @@ function displayQuestions() {
 }
 
 function countdownStart() {
-    console.log("30 second timer started");
     timerID = setInterval(count, 1000);
     $("#message-box").text("");
 }
 
 function stop() {
-
-    console.log("stopping");
     clearInterval(timerID);
-
 }
 
 function count() {
@@ -146,9 +173,11 @@ function count() {
     if (converted === "00:00") {
         stop();
         score--;
-        console.log("Score is now:  " + score);
-        if (questionCounter === 10)
-        {
+
+        playLoseTheme();
+        displayLoseGIF();
+        
+        if (questionCounter === 10) {
             $("#message-box").html("Sorry You ran out of time. Moving to score screen in 5 seconds.");
             setTimeout(endGame, 1000 * 5);
         }
@@ -161,6 +190,7 @@ function count() {
 
 }
 
+// reusing timeConverter function from in-class activity since it is useful to display minutes:seconds easily
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
@@ -193,29 +223,19 @@ function shuffle(a) {
     return a;
 }
 
-
+// Uses bootstrap cards container to generate dynamic container to display the question and answer in the game
 function createCard(article) {
-    // This function takes in a single JSON object for an article/headline
-    // It constructs a jQuery element containing all of the formatted HTML for the
-    // article card
-
-    console.log("createCard function called");
     var answersCombined = [];
 
     questionCounter++;
 
-    console.log("Question #" + questionCounter);
-
-    console.log("question: " + article.question);
-
     var card = $("<div class='card'>");
     var cardHeader = $("<div class='card-header'>").append($("<h6>").append("#" + questionCounter + ": " + article.question));
 
-    console.log("correct answer: " + article.correctAnswer);
-    console.log("incorrect answer: " + article.incorrectAnswer);
-
+    // pushes all the correct and incorrect answers together into one single array so 
+    // that it can be easily listed with a for loop
+    // If else statement test for true/false or multiple choice questions condition
     answersCombined.push(article.correctAnswer);
-    console.log("Showing answersCombined after pushing article.ca:  " + answersCombined);
 
     if (article.incorrectAnswer.length === 1)
         answersCombined.push(article.incorrectAnswer);
@@ -223,28 +243,15 @@ function createCard(article) {
         for (var x = 0; x < article.incorrectAnswer.length; x++)
             answersCombined.push(article.incorrectAnswer[x]);
     }
-    console.log("Showing answersCombined after pushing article.ia:  " + answersCombined);
 
-    for (var x = 0; x < answersCombined.length; x++) {
-        console.log("Loop #" + x + " to see value of answerCombined[" + x + "]!");
-        console.log(answersCombined[x]);
-    }
 
     // If it's a true/false type question, just display the question + true/false answer
     // If there's multiple choice, shuffles the possible answers around in the answersCombined array before displaying the question +  possible answers
     if (answersCombined.length < 3) {
-        console.log("answersCombined.length is : " + answersCombined.length);
-        console.log("Conditional met for having only 2 possible answers.");
-
-        console.log(JSON.stringify(answersCombined[0]));
-        console.log(JSON.stringify(answersCombined[1]));
         var cardBody = $("<div class='card-body'>");
 
-        console.log("Starting for True/False loop to compare answersCombined with article.correctAnswer");
         for (var x = 0; x < answersCombined.length; x++) {
             var paragraph = $("<p>");
-            console.log("testing answersCombined[" + x + "]:  " + answersCombined[x]);
-            console.log("testing article.correctAnswer:  " + article.correctAnswer);
             paragraph.text(answersCombined[x]);
             if (answersCombined[x] === article.correctAnswer)
                 paragraph.attr("id", "correctanswer")
@@ -253,31 +260,20 @@ function createCard(article) {
         }
     }
     else {
-        console.log("Conditional met for having 4 possible answers.");
-        console.log("answersCombined preshuffle:  " + answersCombined);
+        // shuffle function called so that the answer list isn't the same every time
         shuffle(answersCombined);
-        console.log("answersCombined shuffled now:  " + answersCombined);
-
 
         var cardBody = $("<div class='card-body'>");
 
-        console.log("Starting for MultipleChoice loop to compare answersCombined with article.correctAnswer");
         for (var x = 0; x < answersCombined.length; x++) {
             var paragraph = $("<p>");
 
-            console.log("testing answersCombined[" + x + "]:  " + answersCombined[x]);
-            console.log("testing article.correctAnswer:  " + article.correctAnswer);
             paragraph.text(answersCombined[x]);
 
             if (answersCombined[x] === article.correctAnswer)
                 paragraph.attr("id", "correctanswer")
             cardBody.append(paragraph);
         }
-
-        console.log(answersCombined[0]);
-        console.log(answersCombined[1]);
-        console.log(answersCombined[2]);
-        console.log(answersCombined[3]);
     }
 
     card.append(cardHeader, cardBody);
@@ -285,14 +281,17 @@ function createCard(article) {
     return card;
 }
 
+// displays player's score at the end and shows the reset button 
 function endGame() {
     $("#message-box").html("Trivia over! You got " + score + " correct out of 10!");
     $("#reset-button").show();
 }
 
 $(document).ready(function () {
-    $("#info-box").html(startGameText);
+    $("#info-box").html(startGameText + "<br>" + instructionText + "<br>There is sounds so turn on your speakers! :)");
 
+    // start button hides itself when clicked
+    // also unhides the 3 difficulty mode buttons
     $("#start-button").click(function () {
         alert("Time to play the game!!");
         $(this).hide();
@@ -302,6 +301,7 @@ $(document).ready(function () {
         $(".info-text").html("");
     });
 
+    // reset button resets all neccessary global variables and resets/empties neccessary containers
     $("#reset-button").click(function () {
 
         $(this).hide();
@@ -313,19 +313,20 @@ $(document).ready(function () {
             correctAnswer: "",
             incorrectAnswer: []
         }];
+        questionCounter = 0;
 
         timerRemaining = 30;
 
         $("#question-container").empty();
         $("#message-box").html("");
+        $("#timer").html("");
 
         $("#start-button").show();
     });
 
     $("#easyMode").click(function () {
-        console.log("User click easy mode");
         difficulty = "easy";
-        console.log("Difficulty now set to: " + difficulty);
+
         retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
@@ -334,9 +335,8 @@ $(document).ready(function () {
     });
 
     $("#mediumMode").click(function () {
-        console.log("User click medium mode");
         difficulty = "medium";
-        console.log("Difficulty now set to: " + difficulty);
+
         retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
@@ -345,9 +345,8 @@ $(document).ready(function () {
     });
 
     $("#hardMode").click(function () {
-        console.log("User click hard mode");
         difficulty = "hard";
-        console.log("Difficulty now set to: " + difficulty);
+
         retrieveAndParseQA(difficulty);
 
         $("#easyMode").addClass("initiallyHidden");
@@ -356,33 +355,32 @@ $(document).ready(function () {
     });
 
     $("#question-box").on("click", "p", function () {
-
-        console.log("Checking $(<p>).text() value:  " + $(this).text());
-        console.log("Checking questionsArray.correctAnswer value:  " + questionsArray[questionCounter - 1].correctAnswer);
-
         if ($(this).text() === questionsArray[questionCounter - 1].correctAnswer) {
-            console.log("Right answer clicked");
+            playWinTheme();
             stop();
 
-            console.log("Score is now:  " + score);
             $("#message-box").html("Correct answer! Moving to the next question in 10 seconds.");
 
             if (questionCounter === 10)
                 endGame();
-            else
+            else {
+                displayWinGIF();
                 setTimeout(displayQuestions, 1000 * 10);
+            }
         } else {
-            console.log("Wrong answer clicked");
+            playLoseTheme();
             stop();
 
             score--;
-            console.log("Score is now:  " + score);
+
             $("#message-box").html("Wrong answer! The correct answer was " + questionsArray[questionCounter - 1].correctAnswer + ". Moving to the next question in 10 seconds.");
 
             if (questionCounter === 10)
                 endGame();
-            else
+            else {
+                displayLoseGIF();
                 setTimeout(displayQuestions, 1000 * 10);
+            }
         }
     });
 });
